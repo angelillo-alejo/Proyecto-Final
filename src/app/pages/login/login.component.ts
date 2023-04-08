36410865c6d42f7a2845ae6486f8e879faed442f
angelillo-alejo <angelilloalejo@gmail.com>
2023-04-08 22:05:18 +0200

    add cookie

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
+
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = ""
+  password: string = ""
+  error: string = ""
 
+  constructor(private authService: AuthService, private router: Router){}
+
+  login(){
+    this.authService.login(this.email,this.password).subscribe(data=>{
+      //console.log(data)
+      this.error = ""
+      this.authService.setUser(data.user)
+      this.router.navigateByUrl("/perfil")
+    },
+    ex=>{
+      this.error =ex.error
+    })
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,41 +1,53 @@
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
 
-  constructor (private http: HttpClient) {
-    
-   }
+  constructor(private http: HttpClient, private cookies: CookieService) { }
 
-  //Funcion para login
-  login (email: string, password: string){
-
+  login(email: string, password: string): Observable<any>{
+    return this.http.post("http://localhost:3000/login",
+    {
+      email:email,
+      password:password
+    })
   }
 
-  //Funcion para el registro
-  registrar (email: string, password: string, lastname: string, firstname: string) {
-    //el post recibe una URL y un objeto
+  registrar(email:string, password: string, firstname: string, lastname: string){
     return this.http.post("http://localhost:3000/register", 
-    
     {
       email: email,
       password: password,
       firstname: firstname,
-      lastname: lastname
-    })
-
-  }
-  //Funcion para el cierre de sesion
-  logout (){
-
+      lastname: lastname,
+ 
+    }
+    )
   }
 
+  logout(){
+    this.cookies.delete("user")
+  }
 
+  setUser(user: any){
+    this.cookies.set("user",JSON.stringify(user))
+  }
+
+  getUser(){
+    try {
+      return JSON.parse(this.cookies.get("user"))
+    } catch (error) {
+      return null
+    }
+  }
+
+  estaLoggeado():boolean{
+    return this.cookies.check("user")
+  }
 }
-
-// El encargado de  la autenticar user: registro, logueo y cierre de sesion 
-// Tenemos que importar el httpClient
