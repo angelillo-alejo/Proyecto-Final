8a44011a8cc0b9f07387ad199802f9d0edb861bd
angelillo-alejo <angelilloalejo@gmail.com>
2023-04-10 20:11:49 +0200

    add perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,18 @@ import { RegistrarComponent } from './pages/registrar/registrar.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { HomeComponent } from './pages/home/home.component';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 
 
 //Rutas
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  { path: '', component: HomeComponent },
   { path: 'perfil', component: PerfilComponent, canActivate: [AuthGuard] },
-  {path: 'login', component: LoginComponent},
-  {path: 'registrar', component: RegistrarComponent},
-  {path: 'logout', component: LogoutComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard]  },
+  { path: 'registrar', component: RegistrarComponent, canActivate: [AuthGuard] },
+  { path: 'logout', component: LogoutComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
